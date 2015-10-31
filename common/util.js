c9941c8ba04a82330b96175de43479fc23668192
Andrew Straw <strawman@astraw.com>
2015-10-31 23:16:09 +0100

    remove trailing slash from route names

--- common/util.js
+++ common/util.js
@@ -1,7 +1,20 @@
 var data = require('../data')
 
+function removeTrailingSlash( routeName ) {
+  // Remove a trailing slash from routeName, but only if (routeName.length > 1).
+  var fullRoute = routeName;
+  if (fullRoute.length > 1) {
+    var idx = fullRoute.lastIndexOf('/');
+    if (idx == (fullRoute.length-1)) {
+      // has trailing slash
+      fullRoute = fullRoute.slice(0,idx);
+    }
+  }
+  return fullRoute;
+}
+
 function fixRoute( routeName ) {
-  return data.subdir + routeName;
+  return removeTrailingSlash(data.subdir + routeName);
 }
 
 module.exports = {
